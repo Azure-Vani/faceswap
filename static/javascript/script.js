d9fd08778e9cd5d10e9361fe38713ac36b23ef0f
Vani <echooatmeal@gmail.com>
2015-06-15 01:21:19 +0800

    change to long term WebSocket

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,55 +1,56 @@
 $(document).ready(function() {
-  var photobooth = $('#container').photobooth();
-
   if (!window.WebSocket) {
     alert("Browser don't support WebSocket!");
     return;
   }
 
+  var protocol = "ws";
+  if (location.protocol == "https:") {
+    protocol = "wss";
+  }
+  var url = protocol + "://" + location.host + "/ws";
+  console.log("[websocket] established url: " + url);
+  var ws = new ReconnectingWebSocket(url);
+
+  var photobooth;
   var lock = false;
+  ws.onopen = function() {
+    photobooth = $('#container').photobooth();
 
-  photobooth.on('image', function(event, dataUrl) {
-    if (lock) {
-      alert("Please don't send the duplicated request!")
-    }
 
-    $(".result_img").fadeOut(300).delay(500).remove();
-    $(".status").attr("src", "/static/assets/swaping.gif");
-
-    lock = true;
-
-    var protocol = "ws";
-    if (location.protocol == "https:") {
-      protocol = "wss";
-    }
-    var url = protocol + "://" + location.host + "/ws";
-    console.log("[websocket] established url: " + url)
-    var ws = new ReconnectingWebSocket(url);
-
-    ws.onmessage = function(event) {
-      if ($(".result_img").length > 0) {
-        return;
+    photobooth.on('image', function(event, dataUrl) {
+      if (lock) {
+        alert("Please don't send the duplicated request!")
       }
-      var result = JSON.parse(event.data);
-      if (result.action == "finish") {
-        for (var i = 0; i < result.data.length; i++) {
-          console.log("replace " + i);
-          var container = ".lsti" + i;
-          $("<img />", {"src": result.data[i], "class": "result_img", "display":"none"}).appendTo($(container));
-          $('.result_img').fadeIn(400);
+
+      $(".result_img").fadeOut(300).delay(500).remove();
+      $(".status").attr("src", "/static/assets/swaping.gif");
+
+      lock = true;
+
+      ws.onmessage = function(event) {
+        if ($(".result_img").length > 0) {
+          return;
         }
-        lock = false;
-      } else {
-        var newUrl = "/static/assets/" + result.status + ".gif";
-        $('.status').attr("src", newUrl);
+        var result = JSON.parse(event.data);
+        if (result.action == "finish") {
+          for (var i = 0; i < result.data.length; i++) {
+            console.log("replace " + i);
+            var container = ".lsti" + i;
+            $("<img />", {"src": result.data[i], "class": "result_img", "display":"none"}).appendTo($(container));
+          }
+          $('.result_img').fadeIn(400);
+          lock = false;
+        } else {
+          var newUrl = "/static/assets/" + result.status + ".gif";
+          $('.status').attr("src", newUrl);
+        }
       }
-    }
 
-    var data = {"action": "query", "content": dataUrl};
-    ws.onopen = function() {
+      var data = {"action": "query", "content": dataUrl};
       ws.send(JSON.stringify(data));
-    }
 
-  });
+    });
+  }
 
 });
