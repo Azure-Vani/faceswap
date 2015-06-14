46174881d7b227c110268eb5dcac7a828b7994d3
Vincent Zhao <vincentzhaorz@gmail.com>
2015-06-15 00:27:22 +0800

    bug fixed

--- algo/facerepl/facecloning.py
+++ algo/facerepl/facecloning.py
@@ -15,14 +15,19 @@ feature_list = [
 	'right_eye_center',
 	'contour_chin',
 ]
-def get_feature_points(face, w, h):
-	landmark = api.detection.landmark(face_id=face['face_id'], type='83p')
-	result = []
-	for v in feature_list:
-		x = landmark['result'][0]['landmark'][v]['x'] * w / 100
-		y = landmark['result'][0]['landmark'][v]['y'] * h / 100
-		result.append([x,y])
-	return result
+def get_feature_points(face, w, h, dw, dh):
+    landmark = api.detection.landmark(face_id=face['face_id'], type='83p')
+    result = []
+    for v in feature_list:
+        print v
+        x = landmark['result'][0]['landmark'][v]['x'] * w / 100
+        y = landmark['result'][0]['landmark'][v]['y'] * h / 100
+        print x,y
+        x += dw
+        y += dh
+        result.append([x,y])
+        #print result
+    return result
 
 def faceclone(src_name, dst_name):
     src_img = cv2.imread(src_name)
@@ -38,23 +43,56 @@ def faceclone(src_name, dst_name):
     dst_img_height  = dst_rst['img_height']
     dst_face        = dst_rst['face'][0]
 
-    ss = np.array(get_feature_points(src_face, src_img_width, src_img_height), dtype=np.float32)
-    ps = np.array(get_feature_points(dst_face, dst_img_width, dst_img_height), dtype=np.float32)
+       # here we will change our coordinate system
+    # by moving the left top point
+    dw = 100
+    dh = 100
+   
+    nsrc_img = copy_image(src_img, dw, dh)
+    ndst_img = copy_image(dst_img, dw, dh)
+    ss = np.array(get_feature_points(src_face, src_img_width, src_img_height, dw, dh), dtype=np.float32)
+    ps = np.array(get_feature_points(dst_face, dst_img_width, dst_img_height, dw, dh), dtype=np.float32)
+
     map_matrix = cv2.getAffineTransform(ps, ss)
 
-    #dsize = (300,300)
-    map_result = cv2.warpAffine(dst_img, map_matrix, dsize=(src_img_width,src_img_height))
+    nsrc_img_width = src_img_width + dw * 2
+    nsrc_img_height = src_img_height + dh * 2
+    map_result = cv2.warpAffine(ndst_img, map_matrix, dsize=(nsrc_img_width,nsrc_img_height))
     
     extract_mask, center = contour.extract_face_mask(src_face['face_id'], src_img_width, src_img_height, src_name)
-    # merge 
-    ## first blending the border
-    extract_alpha = contour.extract_face_alpha(src_face['face_id'], src_img_width, src_img_height, src_name)
+    next_mask = copy_image(extract_mask, dw, dh)
+   
     center = (map_result.shape[0]/2, map_result.shape[1]/2)
-    map_result = cv2.seamlessClone(src_img, map_result, extract_mask, center, flags=cv2.NORMAL_CLONE)
+    map_result = cv2.seamlessClone(nsrc_img, map_result, next_mask, center, flags=cv2.NORMAL_CLONE)
 
     imap_matrix = cv2.invertAffineTransform(map_matrix)
-    final = cv2.warpAffine(map_result, imap_matrix, dsize=(dst_img.shape[0:2]))
-    return final
+    final = cv2.warpAffine(map_result, imap_matrix, dsize=(ndst_img.shape[0:2]))
+    new_final = copy_back_image(final, dw, dh)
+
+    return new_final
+
+def copy_image(image, dw, dh):
+    img_height, img_width = (image.shape[0], image.shape[1])
+    print "copying image with height %d width %d ..." % (img_height, img_width)
+    new_img = np.zeros((img_height+dh*2, img_width+dw*2, 3), np.uint8)
+    for x in xrange(img_width):
+        for y in xrange(img_height):
+            new_img[dh+y][dw+x] = image[y][x]
+    return new_img
+    #cv2.imshow('newimage', new_img)
+    #cv2.waitKey(0)
+    #new_src_img = np.zeros((src_img_height, src_img_width, 3), np.uint8)
+def copy_back_image(image, dw, dh):
+    img_height, img_width = (image.shape[0], image.shape[1])
+    print "copying back image with height %d width %d ..." % (img_height, img_width)
+    new_img = np.zeros((img_height-dh*2, img_width-dw*2, 3), np.uint8)
+    new_height = img_height-dh*2
+    new_width = img_width-dw*2
+    for x in xrange(new_width):
+        for y in xrange(new_height):
+            new_img[y][x] = image[dh+y][dw+x]
+    return new_img
+
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -76,50 +114,53 @@ if __name__ == '__main__':
     dst_img_height 	= dst_rst['img_height']
     dst_face 		= dst_rst['face'][0]
 
+    # here we will change our coordinate system
+    # by moving the left top point
+    dw = 100
+    dh = 100
+   
+    nsrc_img = copy_image(src_img, dw, dh)
+    ndst_img = copy_image(dst_img, dw, dh)
+    #cv2.imshow('newimage', nsrc_img)
+    #cv2.waitKey(0)
     # there's only one face in this case
-    ss = np.array(get_feature_points(src_face, src_img_width, src_img_height), dtype=np.float32)
-    ps = np.array(get_feature_points(dst_face, dst_img_width, dst_img_height), dtype=np.float32)
+    ss = np.array(get_feature_points(src_face, src_img_width, src_img_height, dw, dh), dtype=np.float32)
+    ps = np.array(get_feature_points(dst_face, dst_img_width, dst_img_height, dw, dh), dtype=np.float32)
 
-    print ps
     print ss
+    print ps
+    #ss = []
     map_matrix = cv2.getAffineTransform(ps, ss)
     print map_matrix
 
     #dsize = (300,300)
-    map_result = cv2.warpAffine(dst_img, map_matrix, dsize=(src_img_width,src_img_height))
+    nsrc_img_width = src_img_width + dw * 2
+    nsrc_img_height = src_img_height + dh * 2
+    print "height=%d width=%d" % (nsrc_img_height, nsrc_img_width)
+    map_result = cv2.warpAffine(ndst_img, map_matrix, dsize=(nsrc_img_width,nsrc_img_height))
     
     extract_mask, center = contour.extract_face_mask(src_face['face_id'], src_img_width, src_img_height, src_name)
-    cv2.imshow('contour', extract_mask)
+    next_mask = copy_image(extract_mask, dw, dh)
+    #cv2.imshow('contour', extract_mask)
     # merge 
-    cv2.imshow('src', src_img)
+    #cv2.imshow('src', src_img)
 
     ## first blending the border
-    extract_alpha = contour.extract_face_alpha(src_face['face_id'], src_img_width, src_img_height, src_name)
-    cv2.imshow('contour', extract_alpha)
-    # merge 
-    #for x in xrange(src_img_width):
-    #    for y in xrange(src_img_height):
-    #        alpha = extract_alpha[y][x] 
-    #        map_result[y][x][0] = (1-alpha) * map_result[y][x][0] + (alpha) * src_img[y][x][0]
-    #        map_result[y][x][1] = (1-alpha) * map_result[y][x][1] + (alpha) * src_img[y][x][1]
-    #        map_result[y][x][2] = (1-alpha) * map_result[y][x][2] + (alpha) * src_img[y][x][2]
-
-    #cv2.imshow('map result', map_result)
+    #extract_alpha = contour.extract_face_alpha(src_face['face_id'], src_img_width, src_img_height, src_name)
+    #cv2.imshow('contour', extract_alpha)
    
-    #center = src_face['position']['nose']
-    #x = center['x'] * src_img_width / 100
-    #y = center['y'] * src_img_height / 100
-    #center = (int(x), int(y))
     center = (map_result.shape[0]/2, map_result.shape[1]/2)
-    map_result = cv2.seamlessClone(src_img, map_result, extract_mask, center, flags=cv2.NORMAL_CLONE)
+    map_result = cv2.seamlessClone(nsrc_img, map_result, next_mask, center, flags=cv2.NORMAL_CLONE)
 
-    cv2.imshow('merge', map_result)
+    #cv2.imshow('merge', map_result)
 
     imap_matrix = cv2.invertAffineTransform(map_matrix)
     print map_result.shape
     print imap_matrix
-    final = cv2.warpAffine(map_result, imap_matrix, dsize=(dst_img.shape[0:2]))
-    cv2.imshow('final.png', final)
-    cv2.imwrite(src_name+dst_name+'final.png', final)
+    final = cv2.warpAffine(map_result, imap_matrix, dsize=(ndst_img.shape[0:2]))
+    new_final = copy_back_image(final, dw, dh)
+
+    cv2.imshow('final.png', new_final)
+    cv2.imwrite(src_name+dst_name+'final.png', new_final)
 
     cv2.waitKey(0)
